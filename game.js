681724aa1e048939ad77b2d61d29f349363fa4ff
Ranjit Ahluwalia <ranjit.ahluwalia@gmail.com>
2015-09-01 19:35:20 -0600

    added reset function and adjusted name display

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var health = 100;
 var name = "Jedi";
 var hits = 0;
+document.getElementById('hits').innerText = hits;
 function slap() {
    health = health - 1;
    health = Math.max(health,0);
@@ -25,5 +26,11 @@ function kick() {
 function update() {
    document.getElementById('health').innerText = health;
    document.getElementById('hits').innerText = hits;
+   document.getElementById('name').innerText = name;
 }
 
+function reset() {
+   hits=0;
+   health = 100;
+   update();
+}
